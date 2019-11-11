90f8ffe546bd15914af08f3f26dc8536d7f5539c
ravi singh <ravisinghofficial@gmail.com>
2019-11-11 21:04:14 +0530

    update: log data

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='drf_tracking_logger',
-    version='1.3',
+    version='1.4',
     author="Ravi Singh",
     author_email="ravisinghofficial@gmail.com",
     description="Log requests on specified logger in JSON format",
